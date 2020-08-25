63f21025e5c316740ff1e40decc81f1f857cb2a7
yunji98 <vkfkddlno@naver.com>
2020-08-25 14:14:24 +0900

    순서 변경 기능 추가

--- C#miniproject/yunji/TodoList/TodoList/Form1.Designer.cs
+++ C#miniproject/yunji/TodoList/TodoList/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
@@ -39,6 +40,8 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // checkedListBox1
@@ -46,7 +49,7 @@
             this.checkedListBox1.BackColor = System.Drawing.SystemColors.Window;
             this.checkedListBox1.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Location = new System.Drawing.Point(54, 107);
+            this.checkedListBox1.Location = new System.Drawing.Point(54, 82);
             this.checkedListBox1.Name = "checkedListBox1";
             this.checkedListBox1.Size = new System.Drawing.Size(371, 260);
             this.checkedListBox1.TabIndex = 0;
@@ -54,7 +57,7 @@
             // button1
             // 
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button1.Location = new System.Drawing.Point(54, 409);
+            this.button1.Location = new System.Drawing.Point(53, 398);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
@@ -65,7 +68,7 @@
             // button2
             // 
             this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button2.Location = new System.Drawing.Point(151, 409);
+            this.button2.Location = new System.Drawing.Point(150, 398);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 2;
@@ -76,7 +79,7 @@
             // button3
             // 
             this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button3.Location = new System.Drawing.Point(252, 409);
+            this.button3.Location = new System.Drawing.Point(251, 398);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 3;
@@ -88,7 +91,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("굴림", 14F);
-            this.label1.Location = new System.Drawing.Point(257, 37);
+            this.label1.Location = new System.Drawing.Point(200, 41);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(91, 19);
             this.label1.TabIndex = 4;
@@ -97,7 +100,7 @@
             // button4
             // 
             this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button4.Location = new System.Drawing.Point(350, 409);
+            this.button4.Location = new System.Drawing.Point(349, 398);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 5;
@@ -108,7 +111,7 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(349, 73);
+            this.checkBox1.Location = new System.Drawing.Point(349, 362);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(76, 16);
             this.checkBox1.TabIndex = 6;
@@ -119,7 +122,7 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("굴림", 14F);
-            this.textBox1.Location = new System.Drawing.Point(111, 32);
+            this.textBox1.Location = new System.Drawing.Point(54, 36);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(36, 29);
             this.textBox1.TabIndex = 7;
@@ -128,7 +131,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("굴림", 14F);
-            this.label2.Location = new System.Drawing.Point(150, 37);
+            this.label2.Location = new System.Drawing.Point(93, 41);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(28, 19);
             this.label2.TabIndex = 9;
@@ -138,7 +141,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("굴림", 14F);
-            this.label3.Location = new System.Drawing.Point(223, 37);
+            this.label3.Location = new System.Drawing.Point(166, 41);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(28, 19);
             this.label3.TabIndex = 11;
@@ -147,16 +150,38 @@
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("굴림", 14F);
-            this.textBox2.Location = new System.Drawing.Point(184, 32);
+            this.textBox2.Location = new System.Drawing.Point(127, 36);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(36, 29);
             this.textBox2.TabIndex = 10;
+            // 
+            // button5
+            // 
+            this.button5.Image = ((System.Drawing.Image)(resources.GetObject("button5.Image")));
+            this.button5.Location = new System.Drawing.Point(454, 181);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(35, 23);
+            this.button5.TabIndex = 12;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Image = ((System.Drawing.Image)(resources.GetObject("button6.Image")));
+            this.button6.Location = new System.Drawing.Point(454, 225);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(35, 23);
+            this.button6.TabIndex = 13;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.ClientSize = new System.Drawing.Size(531, 439);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label2);
@@ -188,6 +213,8 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
 
