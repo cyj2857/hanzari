44eaa2f9bc55b84443eb49a32d8ec45ca9e36e5a
yunji98 <vkfkddlno@naver.com>
2020-09-14 16:15:55 +0900

    views directory 추가 page 관련

---
+++ HanzariPrototype/hanzariProject/frontend/src/views/HancomWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <h1> {{ text }} </h1>
+    <button @click="updateText">Change Text</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      text: 'This is hancom page.'
+    }
+  },
+
+  methods: {
+    updateText () {
+      this.text = 'Click Event Test'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
