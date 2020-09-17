2a9b144c3b7a21568f065c030bc92a5944d51659
yunji98 <vkfkddlno@naver.com>
2020-09-17 10:21:26 +0900

    image 불러오기 성공

--- HanzariPrototype/hanzariProject/frontend/src/components/AttachVue.vue
+++ HanzariPrototype/hanzariProject/frontend/src/components/AttachVue.vue
@@ -2,8 +2,15 @@
   <div>
     <canvas width="100%" height="60%" class="canvas"></canvas>
     <div class="text-center">
-      <v-btn class="ma-2" tile color="indigo" dark @click="clickSave">{{ saveText }}</v-btn>
-      <v-btn class="ma-2" tile outlined color="success" @click="clickImport"> {{ importText }}</v-btn>
+      <input ref="imageInput" type="file" hidden @change= "onChangeImages">
+      <v-btn class="ma-2" tile outlined color="success" @click="clickImport">
+        {{ importText }}
+      </v-btn>
+      <v-img v-if="imageUrl" :src="imageUrl"></v-img>
+      
+      <v-btn class="ma-2" tile color="indigo" dark @click="clickSave">
+        {{ saveText }}
+      </v-btn>
     </div>
   </div>
 </template>
@@ -12,19 +19,28 @@
 export default {
   data () {
     return {
+      importText: '불러오기',
       saveText: '저장',
-      importText: '불러오기'
+      imageUrl: null
     }
   },
   methods: {
+    clickImport () {
+      this.$refs.imageInput.click();
+    },
+    onChangeImages (e) {
+      console.log(e.target.files)
+      const file = e.target.files[0]
+      this.imageUrl = URL.createObjectURL(file)
+    },
     clickSave () {
       this.saveText = '저장완료'
     },
-    clickImport () {
-      this.importText = '불러오기 완료'
+    drawImage () {
+      
     }
   },
-  mounted () {
+  mounted () { //mounted 이후부터 canvas의 dom에 접근 가능
     this.ctx = this.$el.getContext('2d')
   }
 }
