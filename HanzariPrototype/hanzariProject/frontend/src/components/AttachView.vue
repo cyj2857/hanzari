25d6ed914acd77220e14d08042f676151380b729
HANCOM\hancom <cyj2857@naver.com>
2020-09-21 17:45:36 +0900

    캔버스 크기 통일

--- HanzariPrototype/hanzariProject/frontend/src/components/AttachView.vue
+++ HanzariPrototype/hanzariProject/frontend/src/components/AttachView.vue
@@ -13,16 +13,16 @@
 </template>
 
 <script>
-import { eventBus } from '../main.js'
+import { eventBus } from "../main.js";
 export default {
   props: {
     myCanvas: {
-      type: Object,
+      type: Object
       //default: null,
     }
   },
   created() {
-    eventBus.$on('createdRect', (itemName) =>{
+    eventBus.$on("createdRect", itemName => {
       this.makeRectBtn(itemName);
     });
   },
@@ -31,75 +31,74 @@ export default {
       if (this.myCanvas == null) {
         const ref = this.$refs.canvas;
         this.myCanvas = new fabric.Canvas(ref);
-      
       }
     },
     createImage(file) {
-      this.initializing()
-      var image = new Image()
-      var reader = new FileReader()
+      this.initializing();
+      var image = new Image();
+      var reader = new FileReader();
       reader.onload = e => {
         fabric.Image.fromURL(e.target.result, img => {
           img.set({
             scaleX: this.myCanvas.width / img.width,
             scaleY: this.myCanvas.height / img.height
-          })
+          });
           this.myCanvas.setBackgroundImage(
             img,
             this.myCanvas.renderAll.bind(this.myCanvas)
-          )
+          );
           this.myCanvas.renderAll();
-        })
-      }
+        });
+      };
       reader.readAsDataURL(file);
     },
     onFileChange(e) {
-      var files = e.target.files || e.dataTransfer.files
-      if (!files.length) return
-      this.createImage(files[0])
+      var files = e.target.files || e.dataTransfer.files;
+      if (!files.length) return;
+      this.createImage(files[0]);
     },
     makeRectBtn(itemName) {
-      this.initializing()
+      this.initializing();
       var rectangle = new fabric.Rect({
         width: 50,
         height: 50,
         fill: "red",
         opacity: 1
-      })
+      });
       var textObject = new fabric.IText(itemName, {
         left: 0,
         top: 0,
         fontSize: 13,
         fill: "#000000"
-      })
+      });
       var group = new fabric.Group([rectangle, textObject], {
         left: 150,
         top: 150
-      })
-      this.myCanvas.add(group)
+      });
+      this.myCanvas.add(group);
     },
-    makeTextBox () {
-      this.initializing()
+    makeTextBox() {
+      this.initializing();
       var textObject = new fabric.IText("Ma Hyori", {
         left: 0,
         top: 0,
         fontSize: 13,
         fill: "#000000"
-      })
-      this.myCanvas.add(textObject)
+      });
+      this.myCanvas.add(textObject);
     },
     deleteAllBtn() {
-      this.initializing()
+      this.initializing();
       this.myCanvas
         .getObjects()
         .slice()
         .forEach(obj => {
-          this.myCanvas.remove(obj)
-        })
+          this.myCanvas.remove(obj);
+        });
     },
-    saveCanvasBtn () {
-      this.initializing()
-      console.log('svg : ' + this.myCanvas.toSVG())//logs the SVG representation of canvas
+    saveCanvasBtn() {
+      this.initializing();
+      console.log("svg : " + this.myCanvas.toSVG()); //logs the SVG representation of canvas
     }
   }
 };
@@ -113,7 +112,8 @@ export default {
 }
 
 .canvas {
-  margin: 8px;
+  margin-left: 45px;
+  border: 1px solid #000;
   background: aliceblue;
   height: 800px;
   width: 900px;
