264ec35636d5e4294d1756906891f806e1ca781b
AhnHyeonho <ahndding@gmail.com>
2020-09-21 14:44:49 +0900

    controller 테스트코드 수정

--- HanzariPrototype/hanzariProject/src/main/java/com/hancom/hanzariProject/controllers/HomeController.java
+++ HanzariPrototype/hanzariProject/src/main/java/com/hancom/hanzariProject/controllers/HomeController.java
@@ -1,9 +1,6 @@
 package com.hancom.hanzariProject.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Consumer;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -14,59 +11,83 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-import jdk.jshell.Diag;
-import model.Department;
-import model.Employee;
 import model.Figure;
 
 @Controller
 public class HomeController {
 
+	private static SessionFactory sessionFactory;
+	
 	@Autowired
-	private SessionFactory sessionFactory;
+	JdbcTemplate jdbcTemplate;
+	
+	
+	// view first vue.js->vue.js logo
+	@RequestMapping("/")
+	public String Index() {
+		return "indexing";
+	}
 
-   //view first vue.js->vue.js logo 
-   @RequestMapping("/")
-    public String Index() {
-       return "indexing";
-    }
-   
-   //view HanzariPrototype->한컴 화면 
-    @RequestMapping("/test")
-    public String Welcome() {
-       return "welcome";
-    }
-    
-    @GetMapping("/texttest")
-    @ResponseBody
+	// view HanzariPrototype->한컴 화면
+	@RequestMapping("/test")
+	public String Welcome() {
+		return "welcome";
+	}
+
+	@GetMapping("/texttest")
+	@ResponseBody
 	public String Blaa() {
 		System.out.println("texttest");
 		return "hello blaalaa";
 	}
+	
+	@GetMapping("/querytest")
+	@ResponseBody
+	public Iterable<Figure> getAllFigures() {
 		
+		System.out.println("Figure");
+	    List<Figure> result = jdbcTemplate.query(
+	            "SELECT * FROM temp_figures",
+	            (rs, rowNum) -> new Figure(rs.getString("figure_id"), rs.getString("figure_name"))
+	    );
+	    result.forEach(e -> System.out.println(e.getFigure_name().toString()));
+	    return result;
+	}
+	
 	@GetMapping("/testmain")
 	@ResponseBody
 	public void QueryTestMain() {
-		sessionFactory = new Configuration().configure().buildSessionFactory();
+		
+		sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
 		Figure figure = new Figure();
 		figure.setFigure_id("5");
 		figure.setFigure_name("도형5");
 		
+		System.out.println("1");
+
 		Session session = sessionFactory.openSession();
+		System.out.println("2");
 		
 		try {
-		Transaction tx = session.beginTransaction();
-		// ...
-		tx.commit();
-		} catch(Exception e) {
+			Transaction tx = session.beginTransaction();
+			System.out.println("3");
+
+			session.save(figure); // instead of SQL statement
+			System.out.println("4");
+
+			tx.commit();
+			System.out.println("5");
+
+		} catch (Exception e) {
+			System.out.println("6");
 			e.printStackTrace();
+
 		} finally {
 			session.close();
 			sessionFactory.close();
 		}
+		
 	}
 }
