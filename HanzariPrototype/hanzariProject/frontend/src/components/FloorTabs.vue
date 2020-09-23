726afe70f9660802f1c90ca461ed2dced811796b
HANCOM\hancom <cyj2857@naver.com>
2020-09-23 16:34:48 +0900

    update

--- HanzariPrototype/hanzariProject/frontend/src/components/FloorTabs.vue
+++ HanzariPrototype/hanzariProject/frontend/src/components/FloorTabs.vue
@@ -1,65 +1,45 @@
 <template>
   <v-card>
-    <v-card-title>
-      1Floor
-      <v-spacer></v-spacer>
-    </v-card-title>
-    <v-data-table :headers="headers" :items="employees" class="elevation-1">
-      <template v-slot:item.department="{ item }">
-        <v-chip :color="getColor(item.department)" dark>{{ item.department }}</v-chip>
-      </template>
-    </v-data-table>
+    <v-card-text class="text-center">
+      <AttachView></AttachView>
+
+      <v-btn text @click="length--">Remove Tab</v-btn>
+      <v-divider class="mx-4" vertical></v-divider>
+      <v-btn text @click="length++">Add Tab</v-btn>
+    </v-card-text>
+
+    <v-tabs v-model="tab" background-color="cyan" dark>
+      <v-tab v-for="n in length" :key="n" @click="sendFloorInfo(n)">{{ n }} Floor</v-tab>
+    </v-tabs>
   </v-card>
 </template>
 
 <script>
+import { eventBus } from "../main.js";
+import AttachView from "@/components/AttachView.vue";
 export default {
-  data() {
-    return {
-      headers: [
-        {
-          text: "Name",
-          align: "start",
-          sortable: true,
-          value: "name"
-        },
-        { text: "Department", value: "department" },
-        { text: "Number", value: "number" }
-      ],
-      employees: [
-        {
-          name: "Ma Hyori",
-          department: "Development Team",
-          number: "010-5617-4977"
-        },
-        {
-          name: "No Yunji",
-          department: "Development Team",
-          number: "010-4673-2827"
-        },
-        {
-          name: "Choi Yujin",
-          department: "Secure Team",
-          number: "010-7906-3827"
-      },
-      {
-          name: "Kim Dongmin",
-          department: "Engineering Team",
-          number: "010-7906-3827"
-      },
-      {
-          name: "Ahn Hyunho",
-          department: "Engineering Team",
-          number: "010-7906-3827"
-        }
-      ]
-    };
+  components: {
+    AttachView
   },
+  data: () => ({
+    length: 3,
+    tab: null,
+    items: [
+          { tab: 'One', content: 'Tab 1 Content' },
+          { tab: 'Five', content: 'Tab 2 Content' },
+          { tab: 'Six', content: 'Tab 3 Content' }
+        ],
+    floorNum: null,
+  }),
   methods: {
-    getColor(department) {
-      if (department=="Secure Team") return "red";
-       else if (department=="Development Team") return "orange";
-       else if (department=="Engineering Team") return "green";
+    sendFloorInfo (n) {
+      this.floorNum = n
+      eventBus.$emit("changeFloor", this.floorNum)
+    }
+  },
+  watch: {
+    length(val) {
+      this.tab = val - 1;
     }
   }
 };
