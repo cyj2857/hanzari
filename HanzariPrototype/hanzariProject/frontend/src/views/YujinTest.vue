ecabd500c94ed8e9d603076c62a81c2ae8201d1b
HANCOM\hancom <cyj2857@naver.com>
2020-09-23 09:21:58 +0900

    yujin test

--- HanzariPrototype/hanzariProject/frontend/src/views/YujinTest.vue
+++ HanzariPrototype/hanzariProject/frontend/src/views/YujinTest.vue
@@ -35,14 +35,12 @@
 import ListView from "@/components/ListView.vue";
 import MyDataTable from "@/components/MyDataTable.vue";
 import FloorDataTable from "@/components/FloorDataTable.vue";
-import AttachView from "@/components/AttachView.vue";
 import FloorTabs from "@/components/FloorTabs.vue";
 
 export default {
-  name: "HyoriTest",
+  name: "YujinTest",
   components: {
     ListView,
-    AttachView,
     MyDataTable,
     FloorDataTable,
     FloorTabs
