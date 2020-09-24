b3102ae8d404ae2ea2f82b751552ca82538ef1d8
AhnHyeonho <ahndding@gmail.com>
2020-09-24 14:16:10 +0900

    EmployeeController 추가

---
+++ HanzariPrototype/hanzariProject/src/main/java/com/hancom/hanzari/controllers/EmployeeController.java
@@ -0,0 +1,84 @@
+package com.hancom.hanzari.controllers;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.RequiredArgsConstructor;
+import model.Department;
+import model.Employee;
+import model.Seat;
+
+@Api(tags = { "Employee" })
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/api")
+public class EmployeeController {
+
+	private static SessionFactory sessionFactory;
+
+	@ApiOperation(value = "사원 조회", notes = "모든 사원을 조회한다")
+	@GetMapping(value = "/employee")
+	public List<Employee> findAllEmployee() {
+		sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+
+		Session session = sessionFactory.openSession();
+		List<Employee> employee = null;
+		try {
+			Transaction tx = session.beginTransaction();
+
+			Query<Employee> theQuery = session.createQuery("from Employee", Employee.class);
+			employee = theQuery.getResultList();
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		} finally {
+			session.close();
+			sessionFactory.close();
+		}
+
+		return employee;
+	}
+
+	@ApiOperation(value = "사원 입력", notes = "사원을 입력한다.")
+	@PostMapping(value = "/employee")
+	public Employee save(@ApiParam(value = "사원아이디", required = true) @RequestParam String employee_id,
+			@ApiParam(value = "권한", required = true) @RequestParam String authority,
+			@ApiParam(value = "사원이름", required = true) @RequestParam String employee_name,
+			@ApiParam(value = "부서", required = true) @RequestParam Department department,
+			@ApiParam(value = "내선번호", required = false) @RequestParam String extension_number,
+			@ApiParam(value = "좌석", required = false) @RequestParam Seat seat) {
+		sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		Employee employee = Employee.builder().employee_id(employee_id).authority(authority)
+				.employee_name(employee_name).department(department).extension_number(extension_number).build();
+
+		try {
+			Transaction tx = session.beginTransaction();
+
+			session.save(employee);
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		} finally {
+			session.close();
+			sessionFactory.close();
+		}
+		return employee;
+	}
+
+}
