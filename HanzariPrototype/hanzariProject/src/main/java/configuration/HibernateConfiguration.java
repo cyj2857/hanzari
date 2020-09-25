a949afafa200b7998f001852216fd9a172d3ff58
AhnHyeonho <ahndding@gmail.com>
2020-09-25 13:58:45 +0900

    configuration 파일들 패키징

---
+++ HanzariPrototype/hanzariProject/src/main/java/configuration/HibernateConfiguration.java
@@ -0,0 +1,14 @@
+package configuration;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.springframework.context.annotation.Bean;
+
+@org.springframework.context.annotation.Configuration
+public class HibernateConfiguration {
+
+	@Bean
+	public SessionFactory sessionFactory() {
+		return new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+	}
+}
