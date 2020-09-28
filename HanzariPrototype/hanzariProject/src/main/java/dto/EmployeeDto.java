579a9b72b067083cd432e33b5ce2798aa5a1b2e9
AhnHyeonho <ahndding@gmail.com>
2020-09-28 18:27:51 +0900

    DTO 수정중

--- HanzariPrototype/hanzariProject/src/main/java/dto/EmployeeDto.java
+++ HanzariPrototype/hanzariProject/src/main/java/dto/EmployeeDto.java
@@ -1,5 +1,38 @@
 package dto;
 
+import java.util.List;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import model.Employee;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmployeeDto {
 
+	@NotBlank
+	String employee_id;
+
+	@NotBlank
+	String authority;
+
+	@NotBlank
+	String employee_name;
+
+	@NotBlank
+	String department_id;
+
+	String extension_number;
+
+	List<String> seatIdList;
+	
+//	public Employee toEntity() {
+//		new Employee(employee_id, authority, employee_name, extension_number, department_id);
+//	}
 }
--- HanzariPrototype/hanzariProject/src/main/java/dto/SeatDto.java
+++ HanzariPrototype/hanzariProject/src/main/java/dto/SeatDto.java
@@ -1,5 +1,45 @@
 package dto;
 
+import org.hibernate.validator.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import model.Employee;
+import model.Figure;
+import model.Seat;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class SeatDto {
 
+	@NotBlank
+	private String seat_id;
+
+	@NotBlank
+	private BuildingDto building;
+
+	@NotBlank
+	private String floor;
+
+	@NotBlank
+	private Boolean is_group;
+
+	@NotBlank
+	private String group_id;
+
+	@NotBlank
+	private double x;
+
+	@NotBlank
+	private double y;
+
+	@NotBlank
+	private FigureDto figure;
+
+	@NotBlank
+	private String employee_id;
 }
