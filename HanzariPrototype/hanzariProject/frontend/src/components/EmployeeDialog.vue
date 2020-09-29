bb268d10ade4bd4bc7bc387a0b52ccf3001e87ef
yunji98 <vkfkddlno@naver.com>
2020-09-29 11:22:02 +0900

    show different dialog along seat type

--- HanzariPrototype/hanzariProject/frontend/src/components/EmployeeDialog.vue
+++ HanzariPrototype/hanzariProject/frontend/src/components/EmployeeDialog.vue
@@ -2,19 +2,23 @@
   <v-dialog max-width="290" :value="dialog" persistent>
     <v-form ref="form" lazy-validation>
       <v-card>
-        <v-card-title>{{dialogStatus}}</v-card-title>
-        <v-card-title>
-          {{this.employee_name}} Employee Information
-        </v-card-title>
-        <v-card-text>
-          floor : {{this.floor_id}} 
-          id : {{this.employee_id}}
-          name : {{this.employee_name}}
-        </v-card-text>
+        <template v-if="employee_id">
+          <v-card-title>
+            {{ this.employee_name }} Employee Information
+          </v-card-title>
+          <v-card-text>
+            floor : {{ this.floor_id }} id : {{ this.employee_id }} name :
+            {{ this.employee_name }}
+          </v-card-text>
+        </template>
+        <template v-else>
+          <v-card-title>
+            Vacant Seat
+          </v-card-title>
+        </template>
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-btn @click="$emit('close')">Close</v-btn>
-          <v-btn @click="console">test</v-btn>
         </v-card-actions>
       </v-card>
     </v-form>
@@ -22,43 +26,35 @@
 </template>
 
 <script>
-import { eventBus } from '../main'
+import { eventBus } from "../main";
 export default {
   props: {
     dialogStatus: {
-        type: Boolean
-    }
+      type: Boolean,
+    },
   },
-  data () {
+  data() {
     return {
-      employee_id: '',
-      floor_id: '',
-      employee_name: ''
-    }
+      employee_id: "",
+      floor_id: "",
+      employee_name: "",
+    };
   },
   computed: {
-    dialog () {
-      return this.dialogStatus
-    }
+    dialog() {
+      return this.dialogStatus;
+    },
   },
   created() {
-    eventBus.$on('employee_id', employee_id => {
-      this.employee_id = employee_id
+    eventBus.$on("employee_id", (employee_id) => {
+      this.employee_id = employee_id;
     });
-    eventBus.$on('floor_id', floor_id => {
-      this.floor_id = floor_id
+    eventBus.$on("floor_id", (floor_id) => {
+      this.floor_id = floor_id;
     });
-    eventBus.$on('employee_name', employee_name => {
-      this.employee_name = employee_name
-    }); 
-    
+    eventBus.$on("employee_name", (employee_name) => {
+      this.employee_name = employee_name;
+    });
   },
-  methods: {
-    console(){
-      console.log(this.employee_id)
-      console.log(this.floor_id)
-      console.log(this.employee_name)
-    }
-  }
-}
+};
 </script>
