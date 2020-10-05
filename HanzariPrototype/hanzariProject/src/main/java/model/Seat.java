962afe11c583f8eaaf5e9b409ebca90adad50d1e
AhnHyeonho <ahndding@gmail.com>
2020-10-05 12:23:28 +0900

    import문 정리

--- HanzariPrototype/hanzariProject/src/main/java/model/Seat.java
+++ HanzariPrototype/hanzariProject/src/main/java/model/Seat.java
@@ -9,17 +9,13 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators.IntSequenceGenerator;
 
 import dto.SeatDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import model.Figure.FigureBuilder;
 
 @Entity
 @Getter
