4d15fa49b542cbf4d9a160bfbd70d9ebac348dcd
yunji98 <vkfkddlno@naver.com>
2020-10-05 11:01:25 +0900

    make floor dialog

--- HanzariPrototype/hanzariProject/frontend/src/components/FloorTabs.vue
+++ HanzariPrototype/hanzariProject/frontend/src/components/FloorTabs.vue
@@ -1,39 +1,63 @@
 <template>
-  <v-card>
-    <v-card-text class="text-center">
-      <AttachCanvas></AttachCanvas>
-      <v-btn text @click="decreaseTab()">Remove Floor</v-btn>
-      <v-divider class="mx-4" vertical></v-divider>
-      <v-btn text @click="increaseTab()">Add Floor</v-btn>
-    </v-card-text>
-    <v-tabs v-model="floorNum" background-color="cyan" dark>
-      <v-tab v-for="tab of items" :key="tab.id" @change="sendFloorInfo(tab.id)">{{tab.id}}</v-tab>
-      <!-- <v-tab v-for="n in length" :key="n" @change="sendFloorInfo(n)">{{ n }} Floor</v-tab> -->
-    </v-tabs>
-  </v-card>
+  <div>
+    <v-card>
+      <v-card-text class="text-center">
+        <AttachCanvas></AttachCanvas>
+        <v-btn text @click="decreaseTab()">Remove Floor</v-btn>
+        <v-divider class="mx-4" vertical></v-divider>
+        <v-btn text @click="getDialog">Add Floor</v-btn>
+        <!--<v-btn text @click="increaseTab()">Add Floor</v-btn>-->
+      </v-card-text>
+      <v-tabs v-model="floorNum" background-color="cyan" dark>
+        <v-tab
+          v-for="tab of items"
+          :key="tab.id"
+          @change="sendFloorInfo(tab.id)"
+          >{{ tab.id }}</v-tab
+        >
+      </v-tabs>
+    </v-card>
+    <TabEnterDialog
+      :dialogStatus="this.dialogStatus"
+      @close="closeDialog"
+    ></TabEnterDialog>
+  </div>
 </template>
 
 <script>
 import { eventBus } from "../main.js";
 import AttachCanvas from "@/components/AttachCanvas.vue";
+import TabEnterDialog from "@/components/TabEnterDialog.vue";
 export default {
   components: {
-    AttachCanvas
+    AttachCanvas,
+    TabEnterDialog
   },
   data: () => ({
     length: 3,
     tab: null,
-    items: [
-      { id: "One"},
-      { id: "Five"},
-      { id: "Six"}
-    ],
-    floorNum: null
+    items: [{ id: "One" }, { id: "Five" }, { id: "Six" }],
+    floorNum: null,
+    dialogStatus: false
   }),
+  created() {
+    eventBus.$on("close", () => {
+      this.closeDialog()
+    })
+  },
   mounted() {
     this.sendFloorInfo(this.items[0].id);
   },
   methods: {
+    getDialog(){
+      this.dialogStatus = true;
+      console.log(this.dialogStatus);
+    },
+    closeDialog() {
+      console.log("<<<close dialog>>>");
+      this.dialogStatus = false;
+      console.log(this.dialogStatus);
+    },
     sendFloorInfo(n) {
       this.floorNum = n;
       eventBus.$emit("changeFloor", this.floorNum);
@@ -46,12 +70,12 @@ export default {
     increaseTab() {
       this.length++;
       //push
-    }
+    },
   },
   watch: {
     length(val) {
       this.tab = val - 1;
-    }
-  }
+    },
+  },
 };
 </script>
---
+++ HanzariPrototype/hanzariProject/frontend/src/components/TabEnterDialog.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-dialog max-width="290" :value="dialog" persistent>
+    <v-form ref="form" lazy-validation>
+      <v-card>
+        <v-card-title>
+          <span class="headline">Enter Floor</span>
+        </v-card-title>
+        <v-text-field label="Floor" required></v-text-field>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn @click="confirm">Confirm</v-btn>
+          <v-btn @click="$emit('close')">Close</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-form>
+  </v-dialog>
+</template>
+
+<script>
+import { eventBus } from "../main";
+export default {
+  props: {
+    dialogStatus: {
+      type: Boolean,
+    },
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    dialog() {
+      return this.dialogStatus;
+    },
+  },
+  created() {},
+  methods: {
+    confirm() {
+      eventBus.$emit("close");
+    },
+  },
+};
+</script>
