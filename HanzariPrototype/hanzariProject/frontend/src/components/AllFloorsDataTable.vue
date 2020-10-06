b42626fea246d0b70153aeb954234ae3e77673bf
yunji98 <vkfkddlno@naver.com>
2020-10-06 09:27:01 +0900

    axios get seatdata in app.vue

--- HanzariPrototype/hanzariProject/frontend/src/components/AllFloorsDataTable.vue
+++ HanzariPrototype/hanzariProject/frontend/src/components/AllFloorsDataTable.vue
@@ -32,11 +32,8 @@
 <script>
 import { eventBus } from "../main.js";
 import axios from "axios";
-const portNum = 8080;
-const host = "172.30.1.50";
 
 export default {
-
   data() {
     return {
       allEmployeeSeatMap: null,
@@ -69,7 +66,7 @@ export default {
       eventBus.$emit("createSeat", item);
     },
     showSeatButtonClicked(item) {
-      //console.log("?Ç¨?ù¥Ï¶àÎäî Î∞îÎ°úÎ∞îÎ°ú" + this.allEmployeeSeatMap.size);
+      //console.log("allEmployeeSeatMap? size" + this.allEmployeeSeatMap.size);
 
       for (var k = 0; k < this.employees.length; k++) {
         if (this.employees[k].employee_id == item.employee_id) {
@@ -77,18 +74,16 @@ export default {
           //  item.employee_id
           //);
           var eachEmployeeSeatList = this.employees[k].seatIdList;
-          console.log(eachEmployeeSeatList+"Í∞? ?ûêÎ¶? Í∞úÏàò?ù¥?ã§.");
+          console.log(eachEmployeeSeatList+"eachEmployeeSeatList");
 
           console.log(
-            this.employees[k].employee_id +
-              "?ùò ?ûêÎ¶? Í∞úÏàò?äî" +
+            this.employees[k].employee_id + "employee_id? length" +
               eachEmployeeSeatList.length
           ); //4
 
           if (eachEmployeeSeatList) {
             console.log(
-              this.employees[k].employee_id +
-                "?ùò ?ûêÎ¶¨Îäî" +
+              this.employees[k].employee_id + "employee_id? length" +
                 eachEmployeeSeatList[2]
             ); //2
             console.log(this.employees[k].name);
@@ -104,15 +99,12 @@ export default {
     getEmployees() {
       let initEmployeeList = new Array();
       
-      axios.get("http://"+host+":"+portNum+"/employee").then(function(response) {
-        console.log(response.data.length + "?äî ?Ç¨?ù¥Ï¶àÏûÖ?ãà?ã§.");
-
-        
+      axios.get("http://"+host+":"+portNum+"/employee").then(function(response) { 
         for (var i = 0; i < response.data.length; i++) {
           var newEmployee = {};
 
           newEmployee.name = response.data[i].employee_name;
-          console.log(newEmployee.name + "?Ç¥?ù¥Î¶ÑÏù¥?ïº");
+          console.log(newEmployee.name + "???? employee ?? name");
           newEmployee.department = response.data[i].department_name;
           newEmployee.number = response.data[i].extension_number;
           newEmployee.employee_id = response.data[i].employee_id;
@@ -121,7 +113,7 @@ export default {
 
           initEmployeeList.push(newEmployee);
         }
-        console.log(initEmployeeList.length+"?Ç¨?ûå Í∞úÏàò?ïº");
+        console.log(initEmployeeList.length+"employee ???? ?");
         
       });
       return initEmployeeList;
