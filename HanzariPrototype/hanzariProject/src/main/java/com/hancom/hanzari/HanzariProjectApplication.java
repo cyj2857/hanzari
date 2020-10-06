7e4e991538f176cf4358a3a5f363febfb2db5b3f
AhnHyeonho <ahndding@gmail.com>
2020-10-06 14:21:07 +0900

    패키지명 수정으로 인한 ComponentScan 수정

--- HanzariPrototype/hanzariProject/src/main/java/com/hancom/hanzari/HanzariProjectApplication.java
+++ HanzariPrototype/hanzariProject/src/main/java/com/hancom/hanzari/HanzariProjectApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-//@ComponentScan(basePackages = { "com.hancom.hanzari" })
-
 @ComponentScan(basePackageClasses = HanzariProjectApplication.class)
 @EnableJpaRepositories(basePackages = { "com.hancom.hanzari.repository" })
 @SpringBootApplication
