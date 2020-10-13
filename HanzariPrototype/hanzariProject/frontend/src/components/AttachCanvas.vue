4169d62f4018bc1adbd20dcbe2c53e9f33d6e6c7
HANCOM\hancom <cyj2857@naver.com>
2020-10-13 11:23:40 +0900

    addvacant + employee mapping => fix error

--- HanzariPrototype/hanzariProject/frontend/src/components/AttachCanvas.vue
+++ HanzariPrototype/hanzariProject/frontend/src/components/AttachCanvas.vue
@@ -509,23 +509,60 @@ export default {
         }
       }
     },
-    addVacantBtn() {
-      console.log("currnet floor is " + this.currentSelectedFloor);
 
+    addVacantBtn(item) {
       //각 층에 해당하는 도형 리스트 리턴하기
       let eachFloorSeatList = this.getEachFloorSeatList(
         this.currentSelectedFloor
       );
 
+      eventBus.$on("MappingSeat", (item) => {
+        let activeObject = this.floorCanvas.getActiveObject();
+        let shapearray = new Array();
+
+        this.floorCanvas
+          .getObjects()
+          .slice()
+          .forEach((obj) => {
+            shapearray.push(obj);
+          });
+
+        if (activeObject) {
+          shapearray.slice().forEach((obj) => {
+            if (obj == activeObject) {
+              let index = shapearray.indexOf(activeObject);
+              shapearray.splice(index, 1);
+            }
+          });
+          this.floorCanvas.remove(activeObject);
+          eachFloorSeatList.length = 0;
+          this.eachFloorSeatMap.set(this.currentSelectedFloor, shapearray);
+          //console.log("shapearray length: ");
+          //console.log(shapearray.length);
+        }
+
+        this.createSeat(item);
+
+        // console.log("MappingSeat name = " + item.name);
+        // console.log("MappingSeat department = " + item.department);
+        this.floorCanvas.renderAll();
+      });
+      console.log("currnet floor is " + this.currentSelectedFloor);
+
       let rectangle = new fabric.Rect({
         width: 50,
         height: 50,
-        fill: this.getColor(null),
+        fill: "green",
         opacity: 1,
       });
 
       let group = new fabric.Group([rectangle], {
+        floor_id: this.currentSelectedFloor,
         seatId: this.currentSelectedFloor + "-" + this.seatId++, // currentSelectedFloor-seatId
+        //employee_name: item.name,
+        //employee_department: item.department,
+        //employee_number: item.number,
+        //employee_id: item.employee_id,
         left: 150,
         top: 150,
       });
@@ -538,9 +575,17 @@ export default {
         console.log("left = " + x.left); //150
       });
 
+      group.on("mousedblclick", (e) => {
+        let group = e.target;
+        let asObject = group.toObject(["name", "department"]);
+        console.log("dbMappingSeat name = " + asObject.name);
+        console.log("db MappingSeat department = " + asObject.department);
+      });
+
       this.floorCanvas.add(group);
       eachFloorSeatList.push(group);
 
+      console.log("공석 이름 : " + item.name);
       console.log("전체층의 자리 맵 size = " + this.eachFloorSeatMap.size);
       console.log(
         this.currentSelectedFloor +
@@ -603,7 +648,8 @@ export default {
           console.log(res.data);
         });
     },
-    clickLoadBtn() { /*이후에 내부 중복 로직 함수로 뺄 예정 (rectangle, textObject, grouping 과정 및 group의 interaction ) */
+    clickLoadBtn() {
+      /*이후에 내부 중복 로직 함수로 뺄 예정 (rectangle, textObject, grouping 과정 및 group의 interaction ) */
       let eachFloorSeatList = null;
       for (let i = 0; i < this.seats.length; i++) {
         for (let j = 0; j < this.employees.length; j++) {
