11b9c6520575acb65108c2c785c7ce76b79b63b9
yunji98 <vkfkddlno@naver.com>
2020-10-14 15:19:03 +0900

    add previous in changeSeatDialog

--- HanzariPrototype/hanzariProject/frontend/src/components/ChangeSeatDialog.vue
+++ HanzariPrototype/hanzariProject/frontend/src/components/ChangeSeatDialog.vue
@@ -34,6 +34,7 @@
         </template>
         <v-card-actions>
           <v-spacer></v-spacer>
+          <v-btn @click="test">test</v-btn>
           <v-btn @click="confirm">Confirm</v-btn>
           <v-btn @click="$emit('close')">Close</v-btn>
         </v-card-actions>
@@ -42,6 +43,7 @@
   </v-dialog>
 </template>
 
+<!--여기에서 group의 seatId,x,y 변경한 이후에 group에 다시 매핑해주고 attachCanvas로 넘겨줄 것!-->
 <script>
 import { eventBus } from "../main";
 export default {
@@ -52,21 +54,23 @@ export default {
   },
   data() {
     return {
+      previousFloor: null,
+      previousXLocation: null,
+      previousYLocation: null,
+      seatId: null,
       floor: null,
       XLocation: null,
       YLocation: null,
     };
   },
   created() {
-    eventBus.$on("initChangeSeatDialogFloor", (floor) => {
-      this.floor = floor;
-    }),
-      eventBus.$on("initChangeSeatDialogX", (x) => {
-        this.XLocation = x;
-      }),
-      eventBus.$on("initChangeSeatDialogY", (y) => {
-        this.YLocation = y;
-      });
+    eventBus.$on("initChangeSeatDialog", (group) => {
+      let groupToObject = group.toObject(["seatId", "top", "floor_id", "left"]);
+      this.seatId = groupToObject.seatId; // 이후에 random만 올 예정
+      this.previousFloor = groupToObject.floor_id;
+      this.previousXLocation = groupToObject.top;
+      this.previousYLocation = groupToObject.left;
+    });
   },
   computed: {
     dialog() {
@@ -74,8 +78,26 @@ export default {
     },
   },
   methods: {
+    test() {
+      console.log(this.seatId);
+      console.log(this.previousFloor);
+      console.log(this.previousXLocation);
+      console.log(this.previousYLocation);
+    },
     confirm() {
+      if (
+        this.floor == null ||
+        this.XLocation == null ||
+        this.YLocation == null
+      )
+        return;
+
       eventBus.$emit("confirmChangeSeatDialog");
+      /* 받는 곳에서 할 일
+      원래 floor를 넘겨받아 eachFloorSeatMap의 floor에 해당하는 list 가져오고
+      거기에서 group중 seatId가 같은 애를 캔버스에서만 삭제하고 그거 group 정보 복사해와서
+      eachFloorSeatMap에서는 key를 입력 floor로 하여 이동 (되는지는 모르겠음)
+      */
     },
   },
 };
