df9ca68b3cc67faa79cbfda641683f8efbdc4eb4
AhnHyeonho <ahndding@gmail.com>
2020-10-14 15:49:19 +0900

    충돌수정

--- HanzariPrototype/hanzariProject/src/main/java/com/hancom/hanzari/controllers/OldEmployeeController.java
+++ HanzariPrototype/hanzariProject/src/main/java/com/hancom/hanzari/controllers/OldEmployeeController.java
@@ -7,7 +7,6 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
