8303a69178ca3978702f313bb1d04c76326ad146
AhnHyeonho <ahndding@gmail.com>
2020-10-16 14:52:41 +0900

    사번으로 삭제 구현

--- HanzariPrototype/hanzariProject/src/main/java/com/hancom/hanzari/controllers/EmployeeController.java
+++ HanzariPrototype/hanzariProject/src/main/java/com/hancom/hanzari/controllers/EmployeeController.java
@@ -9,8 +9,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +28,7 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeService employeeService;
 
+	// 전체사원 조회
 	@GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE })
 	public ResponseEntity<List<EmployeeDto>> getAllEmps() {
 		List<Employee> employee = employeeService.findAll();
@@ -40,7 +43,7 @@ public class EmployeeController {
 		return new ResponseEntity<EmployeeDto>(employeeService.findById(employee_id).toDto(), HttpStatus.OK);
 	}
 
-	// employee_id로 자리 조회
+	// department_id로 사원 조회
 	@GetMapping(value = "/by-departmentid/{department_id}", produces = { MediaType.APPLICATION_JSON_VALUE })
 	public ResponseEntity<List<EmployeeDto>> getEmpBydepartmentId(@PathVariable("department_id") String department_id) {
 		List<Employee> employee = employeeService.findByDepartmentId(department_id);
@@ -50,7 +53,7 @@ public class EmployeeController {
 		return new ResponseEntity<List<EmployeeDto>>(result, HttpStatus.OK);
 	}
 
-	// keyword로 자리 조회 작성중
+	// keyword로 자리 조회
 	@GetMapping(value = "/keyword/{keyword}", produces = { MediaType.APPLICATION_JSON_VALUE })
 	public ResponseEntity<List<EmployeeDto>> getEmpByKeyword(@PathVariable("keyword") String keyword) throws Exception {
 		keyword = URLDecoder.decode(keyword, "UTF-8"); // 방법1
@@ -58,5 +61,12 @@ public class EmployeeController {
 		List<EmployeeDto> result = new ArrayList<>();
 		employee.forEach(e -> result.add(e.toDto()));
 		return new ResponseEntity<List<EmployeeDto>>(result, HttpStatus.OK);
+	}
+	
+	// employee_id로 삭제
+	@DeleteMapping(value = "/{employee_id}", produces = { MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<Void> deleteEmp(@PathVariable("employee_id") String employee_id) {
+		employeeService.deleteById(employee_id);
+		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
 	}
 }
