1517e03e7a09d002681c4a988a3f79d950ce607a
AhnHyeonho <ahndding@gmail.com>
2020-10-22 13:25:26 +0900

    import문 정리

--- HanzariProject/backend/src/main/java/com/hancom/hanzari/model/Floor.java
+++ HanzariProject/backend/src/main/java/com/hancom/hanzari/model/Floor.java
@@ -2,10 +2,8 @@ package com.hancom.hanzari.model;
 
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
--- HanzariProject/backend/src/main/java/com/hancom/hanzari/model/Seat.java
+++ HanzariProject/backend/src/main/java/com/hancom/hanzari/model/Seat.java
@@ -11,7 +11,6 @@ import javax.persistence.PreRemove;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.hancom.hanzari.dto.SeatDto;
 
 import lombok.AllArgsConstructor;
