25d9e4b4376094032bd552a8f1e61a14b5350b50
dongmin <dongals94@naver.com>
2020-10-26 15:09:47 +0900

    이미지 관련 코드 수정

--- HanzariProject/backend/src/main/java/com/hancom/hanzari/configuration/MinioConfiguration.java
+++ HanzariProject/backend/src/main/java/com/hancom/hanzari/configuration/MinioConfiguration.java
@@ -1,18 +1,22 @@
-/*package com.hancom.hanzari.configuration;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import io.minio.MinioClient;
-import io.minio.errors.InvalidEndpointException;
-import io.minio.errors.InvalidPortException;
-
-@Configuration
-public class MinioConfiguration {
-	@Bean
-	public MinioClient minioClient() throws InvalidEndpointException, InvalidPortException {
-		return new MinioClient().get;
-	}
-}
-*/
+//package com.hancom.hanzari.configuration;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import io.minio.MinioClient;
+//import io.minio.errors.InvalidEndpointException;
+//import io.minio.errors.InvalidPortException;
+//
+//@Configuration
+//public class MinioConfiguration {
+//	@Bean(name="minioClient")
+//	public MinioClient minioClient() throws InvalidEndpointException, InvalidPortException {
+//		// 공식 MinioClient 클래스 사용하기
+//		return new MinioClient("http://172.30.1.56:9000", "AKIAIOSFODNN7EXAMPLE",
+//				"wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY");
+//		//아래 코드는 builder() 에러가 난다. 구글 검색 결과 lombok과 관련된 에러 같지만 해결을 하지 못했다. 다시 확인을 해봐야겠다.
+//		return MinioClient.builder().endpoint("http://172.30.1.56:9000").credentials("AKIAIOSFODNN7EXAMPLE", "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY").build();
+//		
+//	
+//	}
+//}
--- HanzariProject/backend/src/main/java/com/hancom/hanzari/controllers/TestImageController.java
+++ HanzariProject/backend/src/main/java/com/hancom/hanzari/controllers/TestImageController.java
@@ -1,68 +1,90 @@
 package com.hancom.hanzari.controllers;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URLConnection;
 import java.nio.file.Path;
-import java.util.List;
 
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.tomcat.util.http.fileupload.IOUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.jlefebure.spring.boot.minio.MinioException;
 import com.jlefebure.spring.boot.minio.MinioService;
 
-import io.minio.messages.Item;
+import io.minio.MinioClient;
+import io.minio.errors.MinioException;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "*", maxAge = 3600) // CORS 오류 해결하기 위한 어노테이션
 @RestController
 @RequestMapping("api/images")
 public class TestImageController {
 
-	//MinioClient 클래스 사용하기
-    @Autowired
-    private MinioService minioService;
+	// @Autowired
+	private MinioClient minioClient;
 
-    @GetMapping
-    public List<Item> testMinio() throws MinioException {
-        return minioService.list();
-    }
+	// @Autowired
+	private MinioService minioService;
+	// 버킷명
+	String bucketName = "hanzari";
 
-    @GetMapping("/{object}")
-    //한글과컴퓨터-8층.png.png
-    public void getObject(@PathVariable("object") String object, HttpServletResponse response) throws MinioException, IOException {
-        InputStream inputStream = minioService.get(Path.of(object));
-        InputStreamResource inputStreamResource = new InputStreamResource(inputStream);
+	/*
+	 * @GetMapping("/{}") public List<Item> getAllImageFiles(){
+	 * Iterable<Result<Item>> allImageFiles = minioClient.listObjects("hanzari");
+	 * return }
+	 */
 
-        // Set the content type and attachment header.
-        response.addHeader("Content-disposition", "attachment;filename=" + object);
-        response.setContentType(URLConnection.guessContentTypeFromName(object));
+	// 이미지 파일 MinIO 서버에 업로드
+//	@PostMapping
+//	public boolean putImageFile(@RequestParam("iamgeFile") MultipartFile file) {
+//		boolean result = false;
+//
+//		// RequestParam으로 받은 이미지 파일 이름 알아내기
+//		Path path = Path.of(file.getOriginalFilename());
+//
+//		try {
+//			minioClient = new MinioClient("http://172.30.1.56:9000", "AKIAIOSFODNN7EXAMPLE",
+//					"wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY");
+//			// 버킷이 있는지 먼저 검사해주기
+//			boolean found = minioClient.bucketExists(bucketName);
+//			if (found) {
+//				minioClient.putObject(bucketName, file.getOriginalFilename(), file.getInputStream(), file.getSize(),
+//						null, null, file.getContentType());
+//				result = true;
+//			}
+//		} catch (Exception e) {
+//			System.out.println("Error occurred: " + e);
+//		}
+//		return result;
+//	}
 
-        // Copy the stream to the response's output stream.
-        IOUtils.copy(inputStream, response.getOutputStream());
-        response.flushBuffer();
-    }
+//	@GetMapping
+//	public List<Item> testMinio() throws MinioException {
+//		return minioService.list();
+//	}
+//
+//	@GetMapping("/{object}") //한글과컴퓨터-8층.png.png public void
+//	  getObject(@PathVariable("object") String object, HttpServletResponse
+//	  response) throws MinioException, IOException { InputStream inputStream =
+//	  minioService.get(Path.of(object)); InputStreamResource inputStreamResource =
+//	  new InputStreamResource(inputStream);
+//	  
+//	  // Set the content type and attachment header.
+//	  response.addHeader("Content-disposition", "attachment;filename=" + object);
+//	  response.setContentType(URLConnection.guessContentTypeFromName(object));
+//	  
+//	  // Copy the stream to the response's output stream. IOUtils.copy(inputStream,
+//	response.getOutputStream()); response.flushBuffer(); }
 
-    @PostMapping
-    public void addAttachement(@RequestParam("imageData") MultipartFile file) {
-        Path path = Path.of(file.getOriginalFilename());
-        try {
-            minioService.upload(path, file.getInputStream(), file.getContentType());
-        } catch (MinioException e) {
-            throw new IllegalStateException("The file cannot be upload on the internal storage. Please retry later", e);
-        } catch (IOException e) {
-            throw new IllegalStateException("The file cannot be read", e);
-        }
-    }
+	@PostMapping
+	public void addAttachement(@RequestParam("imageFile") MultipartFile file)
+			throws com.jlefebure.spring.boot.minio.MinioException, MinioException {
+		Path path = Path.of(file.getOriginalFilename());
+		try {
+			minioService.upload(path, file.getInputStream(), file.getContentType());
+		} catch (IOException e) {
+			throw new IllegalStateException("The file cannot be read", e);
+		}
+	}
+
 }
