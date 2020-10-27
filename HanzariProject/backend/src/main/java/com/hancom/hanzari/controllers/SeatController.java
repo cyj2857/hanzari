a54b81860c9792cc28ef6a9857cc656f252c9524
AhnHyeonho <ahndding@gmail.com>
2020-10-27 17:25:29 +0900

    Seat에 대한 url 수정 및 일부 메소드 수정

--- HanzariProject/backend/src/main/java/com/hancom/hanzari/controllers/SeatController.java
+++ HanzariProject/backend/src/main/java/com/hancom/hanzari/controllers/SeatController.java
@@ -3,6 +3,8 @@ package com.hancom.hanzari.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -17,11 +19,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hancom.hanzari.dto.SeatDto;
+import com.hancom.hanzari.exception.ResourceNotFoundException;
+import com.hancom.hanzari.model.Building;
 import com.hancom.hanzari.model.Employee;
 import com.hancom.hanzari.model.Figure;
 import com.hancom.hanzari.model.Floor;
 import com.hancom.hanzari.model.Seat;
 import com.hancom.hanzari.model.Shape;
+import com.hancom.hanzari.service.BuildingService;
 import com.hancom.hanzari.service.EmployeeService;
 import com.hancom.hanzari.service.FloorService;
 import com.hancom.hanzari.service.SeatService;
@@ -29,20 +34,32 @@ import com.hancom.hanzari.service.ShapeService;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("api/seats")
+@RequestMapping("api/buildings/{building_id}/floors/{floor_id}/seats")
 public class SeatController {
 
 	@Autowired
 	private SeatService seatService;
 	@Autowired
+	private FloorService floorService;
+	@Autowired
+	private BuildingService buildingService;
+	@Autowired
 	private EmployeeService employeeService;
 	@Autowired
 	private ShapeService shapeService;
-	@Autowired
-	private FloorService floorService;
 
+	@Transactional
 	@GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE })
-	public ResponseEntity<List<SeatDto>> getAllSeats() {
+	public ResponseEntity<List<SeatDto>> getAllSeatsInFloorInBuilding(@PathVariable("building_id") String buildingId,
+			@PathVariable("floor_id") String floorId) throws Exception {
+		Building building = buildingService.findById(buildingId);
+		if (building == null) {
+			throw new ResourceNotFoundException("Building", "building_id", buildingId);
+		}
+		Floor floor = floorService.findById(floorId);
+		if (floor == null) {
+			throw new ResourceNotFoundException("Floor", "floor_id", floorId);
+		}
 		List<Seat> seat = seatService.findAll();
 		List<SeatDto> result = new ArrayList<>();
 		seat.forEach(e -> result.add(e.toDto()));
@@ -50,41 +67,78 @@ public class SeatController {
 	}
 
 	// seat_id로 한 자리 조회
+	@Transactional
 	@GetMapping(value = "/{seat_id}", produces = { MediaType.APPLICATION_JSON_VALUE })
-	public ResponseEntity<SeatDto> getSeat(@PathVariable("seat_id") String seat_id) throws Exception {
-		Seat seat  = seatService.findById(seat_id);
-		return new ResponseEntity<SeatDto>(seat.toDto(), HttpStatus.OK);
+	public ResponseEntity<SeatDto> getSeat(@PathVariable("building_id") String buildingId,
+			@PathVariable("floor_id") String floorId, @PathVariable("seat_id") String seat_id) throws Exception {
+		Building building = buildingService.findById(buildingId);
+		if (building == null) {
+			throw new ResourceNotFoundException("Building", "building_id", buildingId);
+		}
+		Floor floor = floorService.findById(floorId);
+		if (floor == null) {
+			throw new ResourceNotFoundException("Floor", "floor_id", floorId);
+		}
+		return new ResponseEntity<SeatDto>(seatService.findById(seat_id).toDto(), HttpStatus.OK);
 	}
 
 	// employee_id로 자리 조회
 	@GetMapping(value = "/by-empid/{employee_id}", produces = { MediaType.APPLICATION_JSON_VALUE })
-	public ResponseEntity<List<SeatDto>> getSeatsByEmpId(@PathVariable("employee_id") String employee_id) {
-		List<Seat> employee = seatService.findByEmpId(employee_id);
-		List<SeatDto> result = new ArrayList<>();
-		employee.forEach(e -> result.add(e.toDto()));
+	public ResponseEntity<List<SeatDto>> getSeatsByEmpId(@PathVariable("building_id") String buildingId,
+			@PathVariable("floor_id") String floorId, @PathVariable("employee_id") String employeeId) throws Exception {
+
+		Building building = buildingService.findById(buildingId);
+		if (building == null) {
+			throw new ResourceNotFoundException("Building", "building_id", buildingId);
+		}
+		Floor floor = floorService.findById(floorId);
+		if (floor == null) {
+			throw new ResourceNotFoundException("Floor", "floor_id", floorId);
+		}
+		Employee employee = employeeService.findById(employeeId);
+		if (employee == null) {
+			throw new ResourceNotFoundException("Employee", "employee_id", floorId);
+		}
+		List<Seat> seat = seatService.findByEmployee(employee);
+		List<SeatDto> result = new ArrayList<SeatDto>();
+		seat.forEach(e -> result.add(e.toDto()));
 		return new ResponseEntity<List<SeatDto>>(result, HttpStatus.OK);
 	}
 
 	@PostMapping
-	public ResponseEntity<Seat> save(@RequestBody SeatDto seatDto) throws Exception {
-		/*for (Field field : seatDto.getClass().getDeclaredFields()) {
-			field.setAccessible(true);
-			Object value = field.get(seatDto);
-			System.out.println(field.getName() + " : " + value);
-		}*/
+	public ResponseEntity<Seat> save(@PathVariable("building_id") String buildingId,
+			@PathVariable("floor_id") String floorId, @RequestBody SeatDto seatDto) throws Exception {
+		/*
+		 * for (Field field : seatDto.getClass().getDeclaredFields()) {
+		 * field.setAccessible(true); Object value = field.get(seatDto);
+		 * System.out.println(field.getName() + " : " + value); }
+		 */
+
+		Building building = buildingService.findById(buildingId);
+		if (building == null) {
+			throw new ResourceNotFoundException("Building", "building_id", buildingId);
+		}
+		Floor floor = floorService.findById(floorId);
+		if (floor == null) {
+			throw new ResourceNotFoundException("Floor", "floor_id", floorId);
+		}
+		Employee employee = null;
+		if (seatDto.getEmployee_id() != null) {
+			employee = employeeService.findById(seatDto.getEmployee_id());
+			if (employee == null) {
+				throw new ResourceNotFoundException("Employee", "employee_id", floorId);
+			}
+		}
+
+		// Building building = buildingService.findById(seatDto.getBuilding_id());
+		// Floor floor = floorService.findByFloorNameAndBuilding(seatDto.getFloor(),
+		// building);
+		// System.out.println("FLOOR:: " + floor.getFloorId() + " / " +
+		// floor.getFloorName());
 
 		Shape shape = shapeService.findById(seatDto.getShape_id()); // ShapeRepository에서 seatDto의 shape_id를 통해 해당 Shape을
 		Figure figure = Figure.builder().figureId(seatDto.getSeat_id()).shape(shape).width(seatDto.getWidth())
 				.height(seatDto.getHeight()).degree(seatDto.getDegree()).build();
-
-		//Building building = buildingService.findById(seatDto.getBuilding_id());
-		Employee employee = null;
-		//Floor floor = floorService.findByFloorNameAndBuilding(seatDto.getFloor(), building);
-		Floor floor = floorService.findById(seatDto.getFloor());
-		//System.out.println("FLOOR:: " + floor.getFloorId() + " / " + floor.getFloorName());
-		if (seatDto.getEmployee_id() != null) {
-			employee = employeeService.findById(seatDto.getEmployee_id());
-		}
 
 		Seat newSeat = Seat.builder().seatId(seatDto.getSeat_id()).floor(floor).x(seatDto.getX()).y(seatDto.getY())
 				.isGroup(seatDto.getIs_group()).groupId(seatDto.getGroup_id()).employee(employee).figure(figure)
