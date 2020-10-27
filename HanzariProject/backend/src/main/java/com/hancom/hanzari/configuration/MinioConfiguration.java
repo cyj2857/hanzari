e5bcb653740449f82ffbbca0fad632791a3589af
dongmin <dongals94@naver.com>
2020-10-27 17:57:20 +0900

    불필요한 dependency 삭제 및 이미지 컨트롤러 코드 약간 수정

--- HanzariProject/backend/src/main/java/com/hancom/hanzari/configuration/MinioConfiguration.java
+++ HanzariProject/backend/src/main/java/com/hancom/hanzari/configuration/MinioConfiguration.java
@@ -1,5 +1,4 @@
 package com.hancom.hanzari.configuration;
-import org.springframework.beans.factory.annotation.Value;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- HanzariProject/backend/src/main/java/com/hancom/hanzari/controllers/TestImageController.java
+++ HanzariProject/backend/src/main/java/com/hancom/hanzari/controllers/TestImageController.java
@@ -13,9 +13,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.multipart.MultipartFile;
 
 import io.minio.GetObjectArgs;
 import io.minio.MinioClient;
