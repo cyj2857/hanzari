6b26bda947d759e352fd74cd749db4ad1b1448bf
AhnHyeonho <ahndding@gmail.com>
2020-10-28 13:54:01 +0900

    Floor 수정중1

--- HanzariProject/backend/src/main/java/com/hancom/hanzari/model/Floor.java
+++ HanzariProject/backend/src/main/java/com/hancom/hanzari/model/Floor.java
@@ -10,9 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.PrePersist;
 import javax.persistence.PreRemove;
-import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
