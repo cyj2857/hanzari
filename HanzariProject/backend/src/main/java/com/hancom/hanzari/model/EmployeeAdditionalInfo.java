50eb591c198b2d74f3ad23c4c27a3f4d496ace3a
AhnHyeonho <ahndding@gmail.com>
2020-11-04 19:10:21 +0900

    #464 EmployeeAdditionalInfo 의 departmentId, departmentName를 nullable하게 변경

--- HanzariProject/backend/src/main/java/com/hancom/hanzari/model/EmployeeAdditionalInfo.java
+++ HanzariProject/backend/src/main/java/com/hancom/hanzari/model/EmployeeAdditionalInfo.java
@@ -33,12 +33,10 @@ public class EmployeeAdditionalInfo {
 	@Column(name = "extension_number", nullable = true)
 	private String extensionNumber;
 
-	@NotNull
-	@Column(name = "department_id", nullable = false)
+	@Column(name = "department_id", nullable = true)
 	private String departmentId;
 
-	@NotNull
-	@Column(name = "department_name", nullable = false)
+	@Column(name = "department_name", nullable = true)
 	private String departmentName;
 
 	public EmployeeAdditionalInfo() {
