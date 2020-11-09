154b057579c0c753ab0b59e6de678698e171963a
yunji98 <vkfkddlno@naver.com>
2020-11-09 10:11:20 +0900

    #505 와이어프레임 적용할 새 프로젝트 만들기

---
+++ HanzariProject/frontendWireframe/src/views/Hanzari.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hanzari" id="hanzari">
+    한자리는 전체틀 이 안에 컴포넌트들 만들면 된다.
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Hanzari",
+  components: {
+  },
+  data() {
+    return {
+    };
+  },
+  
+};
+</script>
