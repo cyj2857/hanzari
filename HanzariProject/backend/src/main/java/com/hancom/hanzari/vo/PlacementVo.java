9f6b14a15790d1af7cc675e35b7b302ecfaed8ca
AhnHyeonho <ahndding@gmail.com>
2020-11-16 14:13:56 +0900

    #510 csv파일로 업로드할 seatName과 employeeId 연관관계를 정의 및 저장할 VO인 PlacementVo 클래스 생성

---
+++ HanzariProject/backend/src/main/java/com/hancom/hanzari/vo/PlacementVo.java
@@ -0,0 +1,13 @@
+package com.hancom.hanzari.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlacementVo {
+	String seatId;
+	String employeeId;
+}
