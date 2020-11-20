cf8384e2285f3b63ef5aac88f423f0031a541255
dongmin <dongals94@naver.com>
2020-11-20 10:35:38 +0900

    #808 StepC 용도에 맞게 이름 변경 및 각 step에 진입할 때 찍어주는 LOGGER.info 내용 상세하게 변경

--- HanzariProject/backend/src/main/java/com/hancom/hanzari/configuration/TestEmployeeUpdateJobConfiguration.java
+++ HanzariProject/backend/src/main/java/com/hancom/hanzari/configuration/TestEmployeeUpdateJobConfiguration.java
@@ -59,15 +59,15 @@ public class TestEmployeeUpdateJobConfiguration {
 	@Bean
 	//GetEmployeesInfoJob이란 이름으로 Batch Job을 생성
 	//Job의 이름은 별도로 지정하지 않고 Builder를 통해 지정한다.
-	public Job getEmployeesInfoJob(Step fistStep, Step secondStep, Step stepC) {
+	public Job getEmployeesInfoJob(Step fistStep, Step secondStep, Step thirdStep) {
 		return jobBuilderFactory.get("getEmployeesInfoJob").start(fistStep).on(ExitStatus.FAILED.getExitCode()) // FAILED 일 경우
-				.to(stepC) // stepC으로 이동한다.
+				.to(thirdStep) // stepC으로 이동한다.
 				.on("*") // stepC의 결과 관계 없이
 				.end() // stepC으로 이동하면 Flow가 종료한다.
 				.from(fistStep) // stepA로부터
 				.on("*") // FAILED 외에 모든 경우
 				.to(secondStep) // stepB로 이동한다.
-				.next(stepC) // stepB가 정상 종료되면 stepC으로 이동한다.
+				.next(thirdStep) // stepB가 정상 종료되면 stepC으로 이동한다.
 				.on("*") // stepC의 결과 관계 없이
 				.end() // stepC으로 이동하면 Flow가 종료한다.
 				.end() // Job 종료
@@ -127,7 +127,7 @@ public class TestEmployeeUpdateJobConfiguration {
 	//임직원 리스트 받아온 후 프로젝트단 VO 객체에 매핑
 	public Step secondStep() {
 		return stepBuilderFactory.get("secondStep").tasklet((contribution, chunkContext) -> {
-			LOGGER.info(">>>>> 임직원 리스트를 받아온 후 VO 객체에 넣어주는 step");
+			LOGGER.info(">>>>> 발행된 토큰으로부터 API 콜을 통해 JSON형식의 임직원 리스트를 받아온 후 VO 객체에 매핑해주는 step");
 			URL allEmployeeListUrl;
 			HttpsURLConnection allEmployeeListGetConnection;
 			BufferedReader allEmployeeListReader;
@@ -174,9 +174,9 @@ public class TestEmployeeUpdateJobConfiguration {
 
 	@Bean
 	//VO에 매핑된 임직원 리스트를 데이터베이스에 저장
-	public Step stepC() {
-		return stepBuilderFactory.get("stepC").tasklet((contribution, chunkContext) -> {
-			LOGGER.info(">>>>> This is StepC");
+	public Step thirdStep() {
+		return stepBuilderFactory.get("thirdStep").tasklet((contribution, chunkContext) -> {
+			LOGGER.info(">>>>> VO에 매핑된 임직원 리스트를 데이터베이스에 삽입하는 step");
 			resultVo.getAllEmployeeListVo().forEach(e -> {
 				employeeService.save(employeeService.save(Employee.builder().employeeId(e.getEmpId()).authority("viewer")
 				.additionalInfo(EmployeeAdditionalInfo.builder().employeeId(e.getEmpId()).employeeName(e.getUserName()).status("재직")
