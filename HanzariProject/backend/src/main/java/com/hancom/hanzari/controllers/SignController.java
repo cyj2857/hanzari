206bed25ed46ea5cd09059fe5d697e2da05ad650
AhnHyeonho <ahndding@gmail.com>
2020-11-23 16:51:22 +0900

    #834 - 가입과 로그인을 담당하는 SignController 생성 및 제작

---
+++ HanzariProject/backend/src/main/java/com/hancom/hanzari/controllers/SignController.java
@@ -0,0 +1,67 @@
+package com.hancom.hanzari.controllers;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hancom.hanzari.dto.EmployeeDto;
+import com.hancom.hanzari.dto.SeatDto;
+import com.hancom.hanzari.exception.SigninFailedException;
+import com.hancom.hanzari.model.Employee;
+import com.hancom.hanzari.repository.EmployeeRepository;
+import com.hancom.hanzari.service.EmployeeService;
+import com.hancom.hanzari.util.JwtTokenProvider;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.RequiredArgsConstructor;
+
+@Api(tags = { "1. Sign" })
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/v1")
+public class SignController {
+
+	private final EmployeeService employeeService;
+	private final JwtTokenProvider jwtTokenProvider;
+	private final PasswordEncoder passwordEncoder;
+
+	@ApiOperation(value = "로그인", notes = "이메일 회원 로그인을 한다.")
+	@PostMapping(value = "/signin")
+	public String signin(@ApiParam(value = "회원ID : 이메일", required = true) @RequestParam String id,
+			@ApiParam(value = "비밀번호", required = true) @RequestParam String password) throws Exception {
+		Employee employee = employeeService.findById(id);
+		if (!passwordEncoder.matches(password, employee.getPassword()))
+			throw new SigninFailedException();
+
+		return jwtTokenProvider.createToken(String.valueOf(employee.getEmployeeId()), employee.getRoles());
+	}
+
+	@ApiOperation(value = "가입", notes = "회원가입을 한다.")
+	@PostMapping(value = "/signup")
+	public ResponseEntity<Void> signup(@ApiParam(value = "회원ID : 사원번호", required = true) @RequestParam String id,
+			@ApiParam(value = "비밀번호", required = true) @RequestParam String password) {
+		HttpStatus status = null;
+		Employee employee = employeeService.findByIdNullable(id);
+		if (employee != null) {
+			status = HttpStatus.OK;
+		} else {
+			status = HttpStatus.CREATED;
+		}
+
+		employee.setPassword(passwordEncoder.encode(password));
+		employee.setRoles(Collections.singletonList("ROLE_USER"));
+
+		employeeService.save(employee);
+
+		return new ResponseEntity<Void>(status);
+	}
+}
