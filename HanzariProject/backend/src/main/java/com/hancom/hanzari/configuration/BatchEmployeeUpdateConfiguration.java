c55c8051f6108ad9328284be608ca12be9bea2bb
dongmin <dongals94@naver.com>
2020-11-23 10:50:06 +0900

    #837 하나의 라인에 모든 데이터를 갔고 있다는 의미를 주기위해 변수명 jsonAllLine을 jsonOneLine으로 변경

--- HanzariProject/backend/src/main/java/com/hancom/hanzari/configuration/BatchEmployeeUpdateConfiguration.java
+++ HanzariProject/backend/src/main/java/com/hancom/hanzari/configuration/BatchEmployeeUpdateConfiguration.java
@@ -114,17 +114,17 @@ public class BatchEmployeeUpdateConfiguration {
 				tokenBufferedReader = new BufferedReader(new InputStreamReader(tokenCreatedConnection.getInputStream(),
 						"UTF-8"));
 
-				StringBuilder jsonAllLine = new StringBuilder(); //전체 Json라인을 한 줄로 받는 StringBuilder
+				StringBuilder jsonOneLine = new StringBuilder(); //전체 Json라인을 한 줄로 받는 StringBuilder
 				String jsonEachLine; //한 줄씩 받는 String
 				
 				//while문 조건에 jsonEachLine에 readLine 된 것을 대입해주어야한다.
 				//readLine은 다음번 호출할 때 마지막 읽은 다음 줄 부터 읽기에 만약 첫 줄로 끝나는 데이터이고(대부분의 응답받을 Json은 이런 형태일 것 같다.)
 				//while문 안에 대입문을 작성할 경우 NullPointerException이 발생한다.
 				while((jsonEachLine = tokenBufferedReader.readLine()) != null) {		
-					jsonAllLine.append(jsonEachLine);
+					jsonOneLine.append(jsonEachLine);
 				}
 				// jackson 라이브러리를 이용하여 손쉽게 Json형식에서 VO 형식에 매핑해줄 수 있다.
-				tokenVo = new ObjectMapper().readValue(jsonAllLine.toString(), TokenVo.class);
+				tokenVo = new ObjectMapper().readValue(jsonOneLine.toString(), TokenVo.class);
 				tokenBufferedReader.close();
 			} catch (IOException e) {
 				LOGGER.error("IOException in First step", e);
@@ -162,24 +162,24 @@ public class BatchEmployeeUpdateConfiguration {
 				// 따라서 일반적인 방법으로 ObjectMapper의 readValue 메소드를 사용할 수 없다.
 				// 우선 응답받은 original Json을 originalJsonNode 객체에 넣어준다.
 				// token 발행 시에 작성했던 방식과 마찬가지로 한 줄씩 읽어오는 작업을 한다.
-				StringBuilder jsonAllLine = new StringBuilder(); //전체 Json라인을 한 줄로 받는 StringBuilder
+				StringBuilder jsonOneLine = new StringBuilder(); //전체 Json라인을 한 줄로 받는 StringBuilder
 				String jsonEachLine; //한 줄씩 받는 String
 				
 				//while문 조건에 jsonEachLine에 readLine 된 것을 대입해주어야한다.
 				//readLine은 다음번 호출할 때 마지막 읽은 다음 줄 부터 읽기에 만약 첫 줄로 끝나는 데이터이고(대부분의 응답받을 Json은 이런 형태일 것 같다.)
 				//while문 안에 대입문을 작성할 경우 NullPointerException이 발생한다.
 				while((jsonEachLine = allEmployeeListReader.readLine()) != null) {		
-					jsonAllLine.append(jsonEachLine);
+					jsonOneLine.append(jsonEachLine);
 				}
 				
-				JsonNode originalJsonNode = new ObjectMapper().readTree(jsonAllLine.toString());
+				JsonNode originalJsonNode = new ObjectMapper().readTree(jsonOneLine.toString());
 				
 				// forEach문을 돌며 각각의 임직원 정보를 EmployeesVo 객체에 넣어준다.
 				List<EmployeesVo> listEmployeesVo = new ArrayList<>();
 				originalJsonNode.get("result").get("employees").forEach(e -> {
 					// e가 JsonNode 형식이라 readValue()가 아닌 convertValue() 메소드를 사용해야한다.
 					// 내부 로직적으로 setter가 필요하기에 VO 클래스에 @Data 어노테이션을 사용하였다.
-					//jsonAllLine을 사용하여 받아온 Json 데이터를 한 줄로 변환해주었기에 여기서 또 한 줄씩 읽어오는 변환 작업을 할 필요가 없다.
+					//jsonOneLine을 사용하여 받아온 Json 데이터를 한 줄로 변환해주었기에 여기서 또 한 줄씩 읽어오는 변환 작업을 할 필요가 없다.
 					EmployeesVo employeesVo = new ObjectMapper().convertValue(e, EmployeesVo.class);
 					listEmployeesVo.add(employeesVo);
 				});
