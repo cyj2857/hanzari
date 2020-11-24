9d54f1c36cc9e43791cad0a59d44c3e06adfe87b
dongmin <dongals94@naver.com>
2020-11-24 13:34:05 +0900

    #868 putImageFile 메소드안의 InputStream 객체 try with resources statement 사용하여 생성

--- HanzariProject/backend/src/main/java/com/hancom/hanzari/controllers/FloorPlanController.java
+++ HanzariProject/backend/src/main/java/com/hancom/hanzari/controllers/FloorPlanController.java
@@ -71,10 +71,11 @@ public class FloorPlanController {
 			LOGGER.error("Can't find matched floorPlan record. Exception message : ", e);
 		}
 
-		InputStream imagePutInputStream = file.getInputStream();
+		
 		putFloorPlanFileName = new StringBuilder();
 		
-		try {
+		//try with resources statement 사용하여 ()안에서 생성된 객체 try statement 종료시 자동으로 해당 객체의 close() 메소드 호출
+		try(InputStream imagePutInputStream = file.getInputStream();) {
 			//이미지 파일 이름에 일별로 구분해주기 위한 레퍼런스 변수들
 			Date currentTime = new Date();
 			
@@ -101,16 +102,14 @@ public class FloorPlanController {
 				    .build());
 		} catch (Exception e) {
 			LOGGER.error("Can't put object in the MinIO bucket. Exception message : ", e);
-		} finally {
-			//finally안에는 try안에서 리턴문에 의해 메소드가 종료될 경우 그 전에 해야할 작업들을 적어줄 수 있다.
-			//해당 finally문 안에는 후에 try문 안에 return문을 작성할 경우를 대비해 imagePutInputStream을 닫아주어야한다.
-			//하지만 닫아 주는 경우에도 exception이 발생할 가능성이 있다. 그렇게 되면 finally안에 또 try~catch문을 사용해야하는데 이럴 경우 코드가 너무 길어진다.
-			/*TODO 따라서 이러한 경우 IOUtils.closequietly()와 같은 메소드를 사용하여 열어둔 stream을 닫아주고 Null일 경우에도 알아서 처리해주는 라이브러리를 사용해도 좋다.
-			 그러나 이름이 같은 클래스에 대해 해당 라이브러리의 import 경로와 개발 중인 프로젝트의 import 경로가 다른 경우가 있을 수 있고 이런 경우 오류가 날 가능성도 있다.
-			 따라서 이렇게 동일한 작업을 처리해주는 메소드가 필요한 경우 직접 작성을 하고 프로젝트 내에서 필요한 클래스에서 import하는 방법으로 진행하는 것이 좋다.*/
-			if(imagePutInputStream != null)
-				imagePutInputStream.close();
 		}
+		//finally문 작성시 유의사항(해당 try~catch문에서는 try with resources statement를 사용하여 신경쓰지않아도 됨. 학습 차원에서 남겨둠)
+		//finally문 안에는 try문 안에서 리턴문에 의해 메소드가 종료될 경우 그 전에 해야할 작업들을 적어줄 수 있다.
+		//하지만 닫아 주는 경우에도 exception이 발생할 가능성이 있다. 그렇게 되면 finally문 안에 또 try~catch문을 사용해야하는데 이럴 경우 코드가 너무 길어진다.
+		/*TODO 따라서 이러한 경우 IOUtils.closequietly()와 같은 메소드를 사용하여 열어둔 stream을 닫아주고 Null일 경우에도 알아서 처리해주는 라이브러리를 사용해도 좋다.
+		 그러나 이름이 같은 클래스에 대해 해당 라이브러리의 import 경로와 개발 중인 프로젝트의 import 경로가 다른 경우가 있을 수 있고 이런 경우 오류가 날 가능성도 있다.
+		 따라서 이렇게 동일한 작업을 처리해주는 메소드가 필요한 경우 직접 작성을 하고 프로젝트 내에서 필요한 클래스에서 import하는 방법으로 진행하는 것이 좋다.*/
+		
 	}
 
 	//이미지 파일 MinIO 서버에서 다운로드
@@ -151,7 +150,7 @@ public class FloorPlanController {
 			//attachment를 주는 경우에 filename과 함께 주게 되면 Body에 오는 값을 다운로드 받으라는 뜻이다.
 			response.setHeader("Content-disposition","attachment; filename=" + encodedFloorPlanOriginalFileName);
 			//response 객체 자체의 encoding을 UTF-8로 변경
-			//텍스트가 아닌 이미지 파일이기에 깨질 UTF-8 문자가 없지만 학습차원에서 적어두었다.
+			//텍스트가 아닌 이미지 파일이기에 깨질 UTF-8 문자가 없지만 학습 차원에서 적어두었다.
 			response.setCharacterEncoding("UTF-8");
 			response.setContentType(URLConnection.guessContentTypeFromName(getFloorPlanFileName.toString()));
 			IOUtils.copy(imageGetInputStream, response.getOutputStream());
