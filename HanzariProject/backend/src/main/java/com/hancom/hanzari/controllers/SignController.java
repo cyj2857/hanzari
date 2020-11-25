829cc6a31e7ab7b4ef0abc91a7a02e72c504c73b
AhnHyeonho <ahndding@gmail.com>
2020-11-25 17:40:40 +0900

    #907 - CORS 에러 방지를 위한 @CrossOrigin 어노테이션 추가

--- HanzariProject/backend/src/main/java/com/hancom/hanzari/controllers/SignController.java
+++ HanzariProject/backend/src/main/java/com/hancom/hanzari/controllers/SignController.java
@@ -3,6 +3,7 @@ package com.hancom.hanzari.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,6 +20,7 @@ import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 
 @Api(tags = { "1. Sign" })
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/v1")
