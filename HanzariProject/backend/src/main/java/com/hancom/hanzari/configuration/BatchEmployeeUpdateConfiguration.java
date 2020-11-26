5488ddea33c5bbcd8d298587944f3f596bf160fa
dongmin <dongals94@naver.com>
2020-11-26 09:39:21 +0900

    사용하지 않는 import문 정리

--- HanzariProject/backend/src/main/java/com/hancom/hanzari/configuration/BatchEmployeeUpdateConfiguration.java
+++ HanzariProject/backend/src/main/java/com/hancom/hanzari/configuration/BatchEmployeeUpdateConfiguration.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.ArrayList;
